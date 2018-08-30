fb2e1e2c70b3cae18e241bd26054f2468a153052
Chovin Carlson <Chovin@users.noreply.github.com>
2018-08-30 03:56:17 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,9 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
   # puts colors.map {|c| "#{c[0].upcase}: #{c}"}.join ", "
+<<<<<<< HEAD
   puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+=======
+  puts 
+>>>>>>> 65b7461cac255638fd6e1aaf7c7267874f04d73c
 end
